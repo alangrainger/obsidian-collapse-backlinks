9f8400097c0c9e5a16c67cd2fb8af2c2b7d2ddc8
Alan Grainger <alan.grainger@gmail.com>
2025-01-21 15:33:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Automatically collapse backlinks
+
+This plugin will automatically collapse the in-document backlinks
+whenever you open a note.
+
+Simple as that.
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Plugin } from 'obsidian'
+
+export default class CollapseBacklinks extends Plugin {
+  async onload () {
+    this.registerEvent(this.app.workspace.on('file-open', (file) => {
+      if (file) {
+        // Click the icon to collapse the backlinks
+        // @ts-expect-error
+        window.document.querySelector('div.embedded-backlinks [aria-label="Collapse results"]:not(.is-active)')?.click()
+      }
+    }))
+  }
+}
